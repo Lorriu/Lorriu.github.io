f77fcf33ac8ee15729b9d4d37a2907f4f6e04082
Lorraine Ugen <124742429+Lorriu@users.noreply.github.com>
2023-09-29 16:05:10 -0400

    Added the icon change based on city

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-    function formatDate(timestamp){
+function formatDate(timestamp){
         let date = new Date(timestamp);
         let hours = date.getHours();
         let minutes = date.getMinutes();
@@ -20,9 +20,7 @@
         let day = days[date.getDay()];
         return `${day} ${hours}:${minutes}` 
         
-    }
-
-
+}
 
 
 function displayTemperature(response){
@@ -34,26 +32,19 @@ function displayTemperature(response){
     let feelElement = document.querySelector("#feel");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+    let icon =  response.data.condition.icon;
     temperatureElement.textContent = Math.round(response.data.temperature.current);
-    console.log(response.data.temperature.feels_like);
     cityElement.textContent = response.data.city;
     descriptionElement.textContent = response.data.condition.description;
     humidityElement.textContent = Math.round(response.data.temperature.humidity);
     feelElement.textContent = Math.round(response.data.temperature.feels_like);
     windElement.textContent = Math.round(response.data.wind.speed);
     dateElement.textContent = formatDate(response.data.time * 1000)
-
+    iconElement.setAttribute ("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${icon}.png`);
+    iconElement.setAttribute ("alt", response.data.condition.description);
 }
 
-
-
-
-
-function changeIcon(response){
-    let iconElement = document.querySelector("#icon");
-    iconElement.textContent = response.data.condition.icon_url;
-
-}
 
 let apiKey = "toa489c4b0297ba13abfd4fa2587c29d"
 let city = "London"
